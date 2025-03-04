4710b3d70cff442168fa7d845785b3574ac6c719
magewiz <mogollonosorioyamir@gmail.com>
2025-03-04 07:53:16 -0500

    se el buton agregar

---
+++ indes.js
@@ -0,0 +1,7 @@
+const Button  = document.getElementById("remderformodel")
+Button.addEventListener('click' ,  e  =>{
+            console.log("estamos Poniendo Interracion en el back ejd")
+})
+
+
+
